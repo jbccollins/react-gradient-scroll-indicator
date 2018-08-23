9f8e5c0eef5c97eeb72c372a3ba21ad89d3b1e92
James Collins <jbccollins@gmail.com>
2018-08-22 21:48:56 -0400

    Initial Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-gradient-scroll-indicator'
+import GradientScroll from 'react-gradient-scroll-indicator'
 
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div style={{margin: '20px', height: '200px', width: '400px', color: 'white'}}>
+        <GradientScroll
+          primaryColor="#2b2b2b"
+          fadeHeight="20px">
+          
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum lorem massa, in consequat neque rhoncus at. Integer augue diam, vulputate non ex in, sagittis euismod tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean blandit luctus lectus in fringilla. Suspendisse vestibulum sollicitudin leo, quis mollis dolor aliquet et. Maecenas ac laoreet leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce varius mauris massa, a varius risus commodo ac.
+
+          Pellentesque bibendum laoreet dui id ornare. Nullam hendrerit ultricies faucibus. Suspendisse nec eros mollis, eleifend justo non, tristique lorem. Nulla ullamcorper mauris eget velit placerat mollis. Quisque ac ligula sit amet erat blandit blandit. Proin semper erat arcu, sed efficitur diam ultrices ut. Aliquam sollicitudin faucibus congue. Sed elementum ex nec laoreet scelerisque. Proin volutpat eros turpis, ac rhoncus mi varius id. In a orci posuere, congue justo id, dapibus nisi.
+
+          Nulla vulputate diam ut orci sollicitudin euismod. Maecenas quis ex erat. Mauris vitae rhoncus nibh. Sed congue varius mi sit amet ornare. In viverra in libero sit amet egestas. Pellentesque sed gravida dui, sed imperdiet risus. Vivamus nec leo non ante mollis ultricies. Aliquam fermentum purus in erat varius tincidunt.
+
+          Duis ac auctor nunc, vel mollis nulla. Donec egestas blandit odio, sit amet hendrerit dolor. Nam pharetra velit maximus, pharetra enim in, tempor tellus. Nulla sodales eu lectus at vulputate. Suspendisse consectetur lorem ac congue varius. Vivamus vitae magna sagittis, pharetra metus eu, sagittis quam. In nec ligula nec nunc tincidunt vestibulum. Suspendisse aliquet ligula sed volutpat scelerisque. Nunc dictum tempus risus sed laoreet. Nam sagittis mollis mi, quis ultrices leo fermentum quis.
+
+          Pellentesque sagittis ex justo, a ornare lacus fermentum facilisis. Pellentesque id magna in lorem suscipit vehicula at vitae erat. Nam non viverra turpis, nec pretium est. Etiam id velit ut libero feugiat egestas id id risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate semper quam non suscipit. Quisque tristique ac lacus vitae volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus tortor id dolor tincidunt mattis. Phasellus cursus, ante et interdum accumsan, nisl libero commodo dui, consectetur convallis massa tellus tincidunt urna. Cras non felis euismod, pharetra est sit amet, semper arcu. Aliquam sollicitudin nisi sit amet lorem fermentum finibus. Sed finibus sollicitudin neque, et blandit eros sodales sed. Integer maximus scelerisque ex. Ut varius urna lacus, vitae molestie lorem malesuada eu. Maecenas efficitur, nibh fringilla interdum vulputate, ante mauris congue risus, sit amet ornare massa erat non arcu.
+        </GradientScroll>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,76 @@
+// TODO: Handle resizing https://www.npmjs.com/package/react-resize-detector
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Style from 'style-it';
 
 import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
+    primaryColor: PropTypes.string, 
+    fadeColor: PropTypes.string,
+    fadeHeight: PropTypes.string,
+  }
+
+  state = {
+    overlayClasses: []
+  }
+
+  componentDidMount() {
+    this.handleScroll();
+  }
+
+  handleScroll = () => {
+    const scrollableScrollTop = this.scrollableElement.scrollTop;
+    const scrollableHeight = this.scrollableElement.clientHeight;
+    const contentHeight = this.contentElement.clientHeight;
+    const diff = scrollableScrollTop - contentHeight;
+    const atBottom = this.scrollableElement.scrollHeight === contentHeight + scrollableScrollTop;
+    const overlayClasses = [];
+    if (scrollableScrollTop === 0) {
+      overlayClasses.push("top");
+    } 
+    if (atBottom) {
+      overlayClasses.push("bottom");
+    }
+    this.setState({overlayClasses});
   }
 
   render() {
     const {
-      text
+      children,
+      fadeColor,
+      primaryColor,
+      fadeHeight,
     } = this.props
-
+    const { overlayClasses } = this.state;
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+    <Style>
+      {`
+        .${styles.overlay}:before {
+          background: linear-gradient(${primaryColor}, ${fadeColor});
+          height: ${fadeHeight};
+        }
+        .${styles.overlay}:after {
+          background: linear-gradient(${fadeColor}, ${primaryColor});
+          height: ${fadeHeight};
+        }
+     `}
+        <div className={styles.container}>
+          <div className={styles.scrollable} onScroll={this.handleScroll} ref={r => (this.scrollableElement = r)}>
+            <div className={`${styles.overlay} ${overlayClasses.map(s => styles[s])}`}></div>
+            <div className={styles.content} ref={r => (this.contentElement = r)}>
+              {children}
+            </div>
+          </div>
+        </div>
+      </Style>
     )
   }
 }
+
+ExampleComponent.defaultProps = {
+    primaryColor: "#ffffff",
+    fadeColor: "transparent",
+    fadeHeight: "20px",
+};
